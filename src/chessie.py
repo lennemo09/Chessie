867134512dbbc5ead2082615c85a06f5c3d720c7
Nhan Luong <lennemo09@gmail.com>
2020-12-05 18:27:29 +1100

    Added initial driver code.

--- src/chessie.py
+++ src/chessie.py
@@ -5,3 +5,36 @@ MAIN DRIVER FOR CHESSIE.
 @ Chess sprites from: JohnPablok's improved Cburnett chess set.
 @ Found at: https://opengameart.org/content/chess-pieces-and-board-squares
 """
+
+from chessie_engine import *
+import pygame as pg
+
+## STANDARD PYGAME INITIALIZATION
+pg.init()
+
+WINDOW_W = WINDOW_H = 512
+BOARD_SIZE = 8
+
+TILE_SIZE = WINDOW_H // BOARD_SIZE
+SPRITE_SCALE = 0.8 # Percentage of piece sprite occupying the tile (1 = fill entire tile)
+
+FPS = 25
+SPRITES = {}
+
+
+def load_sprites():
+    for piece in pieces_names:
+        #print("Loading piece:","../sprites/pieces/{}.png".format(piece))
+        sprite = pg.image.load("../sprites/pieces/{}.png".format(piece))
+        SPRITES[piece] = pg.transform.scale(sprite, (int(SPRITE_SCALE*TILE_SIZE),int(SPRITE_SCALE*TILE_SIZE)))
+
+
+def main():
+    screen = pg.display.set_mode((WINDOW_W, WINDOW_H))
+    clock = pg.time.Clock()
+    screen.fill(pg.Color("white"))
+
+    state = State()
+    print(state.board)
+
+main()
--- src/chessie_engine.py
+++ src/chessie_engine.py
@@ -3,7 +3,7 @@ ENGINE TO PROCESS CHESSIE'S STATES AND GAME RULES.
 """
 import numpy as np
 
-pieces_name = {
+pieces_full_names = {
     "b_b": "Black Bishop",
     "b_k": "Black King",
     "b_n": "Black Knight",
@@ -18,15 +18,43 @@ pieces_name = {
     "w_r": "White Rook"
 }
 
+pieces_names = ["b_b", "b_k", "b_n", "b_p", "b_q", "b_r", "w_b", "w_k", "w_n", "w_p", "w_q", "w_r"]
+
+pieces_types = ["b","k","n","p","q","r"]
 
 class Piece:
-    def __init__(self,type):
-        self.type = type
-        self.name = pieces_name[type]
+    def __init__(self,name):
+        if name not in pieces_names:
+            raise Exception("Invalid piece name.")
+
+        self.name = name
+
+        self.color = self.get_color(name)
+        self.type = self.get_type(name)
+
+        self.full_name = pieces_full_names[name]
         self.sprite = self.get_sprite()
 
+    def get_color(self,name):
+        if name[0] == 'w':
+            color = 'w'
+            return color
+        elif name[0] == 'b':
+            color = 'b'
+            return color
+        else:
+            raise Exception("Invalid color.")
+
+    def get_type(self,name):
+        if len(name) < 3:
+            return Exception("Invalid name.")
+        type = name[2]
+        if type not in pieces_types:
+            raise Exception("Invalid type.")
+        return type
+
     def get_sprite(self):
-        addr = "/sprites/pieces/{}.png".format(self.type)
+        addr = "../sprites/pieces/{}.png".format(self.type)
         return addr
 
     def __repr__(self):
@@ -42,6 +70,12 @@ class State:
         """
         self.board = self.create_board(player_view)
         self.player_view = player_view
+        self.moving_player = 0 # White moves first
+        self.moves = 0 # Number of moves made so far
+        self.history = [] # Keep track of moves made so far
+
+    def moving_player(self):
+        self.moving_player = moves % 2 # Even number: White's turn, odd number: Black's turn
 
     def create_board(self,player_view=0):
         board = np.array([
