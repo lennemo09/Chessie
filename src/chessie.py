ffca9713ab8b93cc7b9488da4400a14735192325
Nhan Luong <lennemo09@gmail.com>
2020-12-05 19:38:50 +1100

    Can now move pieces illegally.

--- src/chessie.py
+++ src/chessie.py
@@ -18,10 +18,12 @@ WINDOW_W = WINDOW_H = 512
 BOARD_SIZE = 8
 
 TILE_SIZE = WINDOW_H // BOARD_SIZE
-SPRITE_SCALE = 0.8 # Percentage of piece sprite occupying the tile (1 = fill entire tile)
+SPRITE_SCALE = 1.0 # Percentage of piece sprite occupying the tile (1 = fill entire tile)
 
 FPS = 25
 SPRITES = {}
+THEME = 0
+PLAYER = 0 # 0 = White, 1 = Black
 
 themes = ["gray","brown"]
 
@@ -65,19 +67,24 @@ def render_board(screen,theme=0):
             screen.blit(SPRITES[tile], pg.Rect(col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
 
-
-
 def render_tiles(screen,board):
     """
     Render the tiles with the correct pieces on it.
     """
-    pass
+    for row in range(BOARD_SIZE):
+        for col in range(BOARD_SIZE):
+            piece = board[row,col]
+
+            if piece != "---":
+                piece_name = piece.name
+                screen.blit(SPRITES[piece_name], pg.Rect(col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+
 
 def draw_board(screen,state):
     """
     Render graphics.
     """
-    render_board(screen)
+    render_board(screen,THEME)
     render_tiles(screen,state.board)
 
 
@@ -90,15 +97,39 @@ def main():
     load_sprites()
 
     running = True
+
+    selected = () # User's last seletec tile (row,col)
+    selection_buffer = [] # Store user's last selected tiles [src,dst]
+
     while running:
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 running = False
+            elif e.type == pg.MOUSEBUTTONDOWN:
+                pos = pg.mouse.get_pos() # Add offset if extra GUI panels are added (mouse coord needs to be relative to the board's borders, not window's)
 
+                col = pos[0] // TILE_SIZE
+                row = pos[1] // TILE_SIZE
+
+                if (row,col) == selected: # Selected the same tile twice, deselect
+                    selected = ()
+                    selection_buffer = []
+                else:
+                    selected = (row,col)
+                    selection_buffer.append(selected)
+
+                if len(selection_buffer) == 2: # 2 tiles selected -> Move
+                    move = Move(selection_buffer[0], selection_buffer[1], state.board)
+                    print(move.get_notation())
+                    state.move_piece(move)
+                    selected = ()
+                    selection_buffer = []
+
+        draw_board(screen,state)
         clock.tick(FPS)
         pg.display.flip()
 
-        render_board(screen)
+
 
 if __name__ == "__main__":
     main()
--- src/chessie_engine.py
+++ src/chessie_engine.py
@@ -63,6 +63,57 @@ class Piece:
     def __str__(self):
         return self.name
 
+
+class Move:
+    """
+    Chess (rank-file) notations:
+    Ranks := Rows (1-8)
+    Files := Colums (a-h)
+    """
+    ranks_to_rows = [{"1": 7, "2": 6, "3": 5, "4": 4,
+                      "5": 3, "6": 2, "7": 1, "8": 0},
+                     {"1": 0, "2": 1, "3": 2, "4": 3,
+                      "5": 4, "6": 5, "7": 6, "8": 7}]
+
+    rows_to_ranks = [{x: y for y, x in ranks_to_rows[0].items()},
+                     {x: y for y, x in ranks_to_rows[1].items()}]
+
+    files_to_cols = [{"a": 0, "b": 1, "c": 2, "d": 3,
+                      "e": 4, "f": 5, "g": 6, "h": 7},
+                     {"a": 7, "b": 6, "c": 5, "d": 4,
+                      "e": 3, "f": 2, "g": 1, "h": 0}]
+
+    cols_to_files = [{x: y for y, x in files_to_cols[0].items()},
+                     {x: y for y, x in files_to_cols[1].items()}]
+
+
+
+    def __init__(self,src,dst,board,player=0):
+        """
+        Using Move class to have convenient data storage with each move.
+        """
+        self.player = player # 0: white, 1: player
+        self.src_row = src[0]
+        self.src_col = src[1]
+        self.dst_row = dst[0]
+        self.dst_col = dst[1]
+
+        self.piece = board[self.src_row,self.src_col]
+        self.capture = board[self.dst_row,self.dst_col]
+
+    def get_notation(self):
+        """
+        Return Rank-File notation of the move.
+        """
+        return self.get_tile(self.src_row,self.src_col) + self.get_tile(self.dst_row,self.dst_col)
+
+    def get_tile(self,row,col):
+        """
+        Return Rank-File notation of the tile.
+        """
+        return self.cols_to_files[self.player][col] + self.rows_to_ranks[self.player][row]
+
+
 class State:
     def __init__(self,player_view=0):
         """
@@ -74,18 +125,40 @@ class State:
         self.moves = 0 # Number of moves made so far
         self.history = [] # Keep track of moves made so far
 
-    def moving_player(self):
-        self.moving_player = moves % 2 # Even number: White's turn, odd number: Black's turn
+    def get_moving_player(self):
+        self.moving_player = self.moves % 2 # Even number: White's turn, odd number: Black's turn
 
     def create_board(self,player_view=0):
-        board = np.array([
-        [Piece("b_r"),Piece("b_n"),Piece("b_b"),Piece("b_k"),Piece("b_q"),Piece("b_b"),Piece("b_n"),Piece("b_r")],
-        [Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p")],
-        ["---","---","---","---","---","---","---","---"],
-        ["---","---","---","---","---","---","---","---"],
-        ["---","---","---","---","---","---","---","---"],
-        ["---","---","---","---","---","---","---","---"],
-        [Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p")],
-        [Piece("w_r"),Piece("w_n"),Piece("w_b"),Piece("w_k"),Piece("w_q"),Piece("w_b"),Piece("w_n"),Piece("w_r")]
-        ])
+        if player_view == 0:
+            board = np.array([
+            [Piece("b_r"),Piece("b_n"),Piece("b_b"),Piece("b_k"),Piece("b_q"),Piece("b_b"),Piece("b_n"),Piece("b_r")],
+            [Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p")],
+            ["---","---","---","---","---","---","---","---"],
+            ["---","---","---","---","---","---","---","---"],
+            ["---","---","---","---","---","---","---","---"],
+            ["---","---","---","---","---","---","---","---"],
+            [Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p")],
+            [Piece("w_r"),Piece("w_n"),Piece("w_b"),Piece("w_k"),Piece("w_q"),Piece("w_b"),Piece("w_n"),Piece("w_r")]
+            ])
+        else:
+            board = np.array([
+            [Piece("w_r"),Piece("w_n"),Piece("w_b"),Piece("w_k"),Piece("w_q"),Piece("w_b"),Piece("w_n"),Piece("w_r")],
+            [Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p"),Piece("w_p")],
+            ["---","---","---","---","---","---","---","---"],
+            ["---","---","---","---","---","---","---","---"],
+            ["---","---","---","---","---","---","---","---"],
+            ["---","---","---","---","---","---","---","---"],
+            [Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p"),Piece("b_p")],
+            [Piece("b_r"),Piece("b_n"),Piece("b_b"),Piece("b_k"),Piece("b_q"),Piece("b_b"),Piece("b_n"),Piece("b_r")]
+            ])
         return board
+
+    def move_piece(self,move):
+        """
+        Castle not handled.
+        """
+        self.board[move.src_row,move.src_col] = "---"
+        self.board[move.dst_row,move.dst_col] = move.piece
+        self.history.append(move) # Added move to log
+        self.moves += 1
+        self.moving_player = self.get_moving_player()
